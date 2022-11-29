51fdb07ff55a634c3ce82eb779881c23a7ede5ff
Enfield-Li <1076462153@qq.com>
2022-11-30 04:53:38 +0800

    Update

--- client/src/component/layout/subNavbar/space/SpaceList.tsx
+++ client/src/component/layout/subNavbar/space/SpaceList.tsx
@@ -9,7 +9,7 @@ type Props = { space: SpaceType };
 export default memo(SpaceList);
 function SpaceList({ space }: Props) {
   return (
-    <motion.div layout="position" transition={{ duration: 0.12 }}>
+    <motion.div layout="position" transition={{ duration: 0.2 }}>
       <Space space={space} />
 
       {space.allListOrFolder.map((folder) => (
@@ -17,7 +17,7 @@ function SpaceList({ space }: Props) {
           key={folder.id}
           hidden={!space.isOpen}
           animate={{ opacity: space.isOpen ? 1 : 0 }}
-          transition={{ type: "keyframes", duration: 0.1 }}
+          transition={{ type: "keyframes", duration: 0.3 }}
         >
           <FolderAndList spaceId={space.id} folder={folder} />
         </motion.div>
