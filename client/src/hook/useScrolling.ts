6106fa2ec364c729308bd4d4e04fe4b07e951407
Enfield-Li <1076462153@qq.com>
2023-01-30 10:21:08 +0800

    Utils

---
+++ client/src/hook/useScrolling.ts
@@ -0,0 +1,27 @@
+import React, { useEffect, useState } from "react";
+
+type Props = {};
+
+export default function useScrolling({}: Props) {
+  const [isScrolling, setIsScrolling] = useState(false);
+
+  useEffect(() => {
+    let timeout: NodeJS.Timeout;
+
+    const handleScroll = () => {
+      setIsScrolling(true);
+      clearTimeout(timeout);
+      timeout = setTimeout(() => {
+        setIsScrolling(false);
+      }, 100);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  return isScrolling;
+}
